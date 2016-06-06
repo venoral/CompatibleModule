767319d00a149277370fd630ad09770f9b50cd69
venoral <xx294874279@qq.com>
2016-06-06 14:29:16 +0800

    event事件对象

--- Events/event.js
+++ Events/event.js
@@ -4,7 +4,7 @@ var EventUtil = {
    * @param {[string]} type [事件名称]
    * @param {[function]} handler [事件处理程序函数]
    */
-  addHandler : function(ele, type, handler){
+  addHandler : function (ele, type, handler){
     if(ele.addEventListener){
       ele.addEventListener(type, handler, false);
     }else if(ele.attachEvent){
@@ -13,7 +13,7 @@ var EventUtil = {
       ele['on' + type] = handler;
     }
   },
-  removeHandler : function(ele, type, handler){
+  removeHandler : function (ele, type, handler){
     if(ele.removeEventListener){
       ele.removeEventListener(type, handler);
     }else if(ele.detachEvent){
@@ -21,6 +21,38 @@ var EventUtil = {
     }else{
       ele['on' + type] = null;
     }
-  }
+  },
 
-}
+  /**
+   * @param  {[obj]} event [事件对象]
+   * @return {[obj]} event [事件对象]
+   */
+  getEvent : function (event){
+     return event ? event : window.event;
+  },
+  /**
+   * @param  {[obj]} event [事件对象]
+   * @return {[obj]} target [目标对象]
+   */
+  getTarget : function (event) {
+     return event.target || event.srcElement;
+  },
+  /**
+   * @param  {[obj]} 事件对象
+   */
+  preventDefault : function (event) {
+     if( event.preventDefault ){
+        event.preventDefault();
+     }else{
+        event.returnValue = false;
+     }
+  },
+  stopPropagation : function (event) {
+     if( event.stopPropagation ){
+        event.stopPropagation();
+     }else{
+        event.cancelBubble = true;
+     }
+  }
+  
+};
